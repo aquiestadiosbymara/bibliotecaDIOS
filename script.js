ff4e63b7435a64aa6b89f3738890482bdefacb34
aquiestadiosbymara <aquiestadiosbymara@hotmail.com>
2026-09-16 14:38:32 +0200

    Add comenzarRecurso function with alert message

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function entrarBiblioteca() {
     alert("La biblioteca estará disponible muy pronto.");
 }
+function comenzarRecurso() {
+    alert("Aquí comenzará el Código de la Determinación.");
+}
